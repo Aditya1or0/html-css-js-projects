2051b532148d2b9a2a0e1a120d50985c937ead18
Aditya Sharma <114848909+Aditya1or0@users.noreply.github.com>
2024-10-10 20:51:25 +0530

    Add files via upload

---
+++ Drag&Drop/script.js
@@ -0,0 +1,23 @@
+let lists = document.getElementsByClassName("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for (let list of lists) {
+  list.addEventListener("dragstart", function (event) {
+    let selected = event.target;
+    rightBox.addEventListener("dragover", function (event) {
+      event.preventDefault();
+    });
+    rightBox.addEventListener("drop", function (event) {
+      rightBox.appendChild(selected);
+      selected = null;
+    });
+    leftBox.addEventListener("dragover", function (event) {
+      event.preventDefault();
+    });
+    leftBox.addEventListener("drop", function (event) {
+      leftBox.appendChild(selected);
+      selected = null;
+    });
+  });
+}
